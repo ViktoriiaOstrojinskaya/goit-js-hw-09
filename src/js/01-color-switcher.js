7f4de28e6f0f3ad48c2865f985723c4d72536673
Viktoriia Ostrojinskaya <viktoriia.ostrojinskaya@gmail.com>
2022-10-05 06:34:43 -0400

    task #1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -7,16 +7,20 @@ const startBnt = document.querySelector('button[data-start]');
 const stopBtn = document.querySelector('button[data-stop]');
 let timeId = null;
 
+stopBtn.disabled = true;
+
 startBnt.addEventListener('click', onStartBtn);
 
 function onStartBtn() {
   timeId = setInterval(() => {
     body.style.backgroundColor = getRandomHexColor();
     startBnt.disabled = true;
+    stopBtn.disabled = false;
   }, 1000);
 }
 
 stopBtn.addEventListener('click', () => {
   clearInterval(timeId);
+  stopBtn.disabled = true;
   startBnt.disabled = false;
 });
