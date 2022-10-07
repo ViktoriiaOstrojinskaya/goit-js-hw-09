a8ac616aef05afda6525c6526414698c2c39d70b
Viktoriia Ostrojinskaya <viktoriia.ostrojinskaya@gmail.com>
2022-10-07 19:50:07 -0400

    task #2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,6 +2,7 @@ import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
 const buttonStart = document.querySelector('button[data-start]');
+buttonStart.disabled = true;
 console.log(buttonStart);
 
 const fp = flatpickr('#datetime-picker', {
@@ -10,6 +11,36 @@ const fp = flatpickr('#datetime-picker', {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    selectedDates.getTime();
+    if (selectedDates[0] < new Date()) {
+      window.alert('Please choose a date in the future');
+    } else {
+      buttonStart.disabled = false;
+    }
   },
 });
+
+buttonStart.addEventListener('click', onStartBtn);
+
+function onStartBtn() {}
+
+// function convertMs(ms) {
+//   // Number of milliseconds per unit of time
+//   const a = new Date() - selectedDates[0];
+
+//   const second = 1000;
+//   const minute = second * 60;
+//   const hour = minute * 60;
+//   const day = hour * 24;
+
+//   // Remaining days
+//   const days = Math.floor(ms / day);
+//   // Remaining hours
+//   const hours = Math.floor((ms % day) / hour);
+//   // Remaining minutes
+//   const minutes = Math.floor(((ms % day) % hour) / minute);
+//   // Remaining seconds
+//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+//   return { days, hours, minutes, seconds };
+// }
