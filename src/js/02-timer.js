6210e9ef1fdf58809a16bf5ad398081df635cee1
Viktoriia Ostrojinskaya <viktoriia.ostrojinskaya@gmail.com>
2022-10-07 20:20:58 -0400

    task #2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -19,28 +19,3 @@ const fp = flatpickr('#datetime-picker', {
     }
   },
 });
-
-// buttonStart.addEventListener('click', onStartBtn);
-
-// function onStartBtn() {}
-
-// function convertMs(ms) {
-//   // Number of milliseconds per unit of time
-//   const a = new Date() - selectedDates[0];
-
-//   const second = 1000;
-//   const minute = second * 60;
-//   const hour = minute * 60;
-//   const day = hour * 24;
-
-//   // Remaining days
-//   const days = Math.floor(ms / day);
-//   // Remaining hours
-//   const hours = Math.floor((ms % day) / hour);
-//   // Remaining minutes
-//   const minutes = Math.floor(((ms % day) % hour) / minute);
-//   // Remaining seconds
-//   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-//   return { days, hours, minutes, seconds };
-// }
