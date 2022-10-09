569c4220beee41873d4cd1c26d15eeafbfd5e3a6
Viktoriia Ostrojinskaya <viktoriia.ostrojinskaya@gmail.com>
2022-10-09 00:44:55 -0400

    task#3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,22 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  form: document.querySelector('form'),
+  delay: document.querySelector('input[name=delay]'),
+  step: document.querySelector('input[name=step]'),
+  amount: document.querySelector('input[name=amount]'),
+};
+
+refs.form.addEventListener('submit', createPromise);
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  setTimeout(() => {
+    if (shouldResolve) {
+      // Fulfill
+      // const promise = new Promise(() => {});
+    } else {
+      // Reject
+    }
+  }, `${refs.delay.value}`);
 }
